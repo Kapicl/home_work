f8367047f050d1e7d9f5876c42ed8ace74319518
Kapicl <sanylemon4ek@gmail.com>
2024-11-27 18:57:07 +0300

    da

--- 3/world.py
+++ 3/world.py
@@ -2,6 +2,8 @@ import texture
 from tkinter import NW
 from random import randint, choice
 
+from DM import canvas
+
 GROUND = 'g'
 WATER = 'w'
 CONCRETE = 'c'
@@ -12,16 +14,41 @@ _camera_x = 0
 _camera_y = 0
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-WIDTH = SCREEN_WIDTH * 6
-HEIGHT = SCREEN_HEIGHT * 4
 _map = None
+
+def get_rows():
+    return len(_map)
+
+def get_cols():
+    return len(_map[0])
+
+def get_width():
+    return get_cols() * BLOCK_SIZE
+
+def get_height():
+    return get_rows() * BLOCK_SIZE
+
+def create_map(rows=20, cols=20):
+    global _map
+    _map = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            block = GROUND
+            if  i == 0 or j == 0 or i == rows - 1 or j == cols - 1:
+                block = CONCRETE
+            elif randint(1, 100) <= 15:
+                block = choice([BRICK, WATER, CONCRETE])
+            cell = _Cell(_canvas, block, j * BLOCK_SIZE, i * BLOCK_SIZE)
+            row.append(cell)
+        _map.append(row)
 
 
 def initialaze(canv):
     global _canvas, _map
     _canvas = canv
     _map = _Cell(_canvas, WATER, 0, 0)
-    #create_map(20, 20)
+    create_map(20, 20)
 
 def set_camera_xy(x, y):
     global _camera_x, _camera_y
@@ -29,10 +56,10 @@ def set_camera_xy(x, y):
         x = 0
     if y < 0:
         y = 0
-    if x > WIDTH - SCREEN_WIDTH:
-        x = WIDTH - SCREEN_WIDTH
-    if y > HEIGHT - SCREEN_HEIGHT:
-        y = HEIGHT - SCREEN_HEIGHT
+    if x > get_width() - SCREEN_WIDTH:
+        x = get_width() - SCREEN_WIDTH
+    if y > get_height() - SCREEN_HEIGHT:
+        y = get_height() - SCREEN_HEIGHT
     _camera_x = x
     _camera_y = y
 
@@ -44,6 +71,11 @@ def get_screen_x(world_X):
 
 def get_screen_y(world_Y):
     return world_Y - _camera_y
+
+def update_cell(row, col):
+    if row < 0 or col < 0 or row >= get_rows() or col >= get_cols():
+        return
+    _map[row][col].update()
 
 class _Cell:
     def __init__(self, canvas, block,x,y):
@@ -65,4 +97,17 @@ class _Cell:
 
     def get_block(self):
         return self.__block
+
+    def update(self):
+        if self.__block == GROUND:
+            return
+        screen_x = get_screen_x(self.__x)
+        screen_y = get_screen_y(self.__y)
+        self.__canvas.moveto(self.__id, x=screen_x, y=screen_y)
+
+def update_map():
+    for i in range(0, get_rows()):
+        for j in range(0, get_cols()):
+            update_cell(i, j)
+
 
